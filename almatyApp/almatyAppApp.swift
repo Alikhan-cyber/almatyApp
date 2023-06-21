b611c0316f1b10e2f770a7d1a10e5df162feab9c
Alikhan-cyber <kzalehandro@gmail.com>
2023-06-21 21:49:15 +0600

    Initial Commit

---
+++ almatyApp/almatyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  almatyAppApp.swift
+//  almatyApp
+//
+//  Created by Alikhan Abdikhalyk on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct almatyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
